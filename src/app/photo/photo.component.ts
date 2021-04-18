4a9683bb5957e193399b91dbd1cde80362e9c2d5
Reginaldo Sena <regsena@gmail.com>
2021-04-18 09:54:10 -0400

    iniciando os trabalhos

---
+++ src/app/photo/photo.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "ap-photo",
+    templateUrl: "photo.component.html"
+})
+export class PhotoComponent {
+    description = 'Leão';
+    url='https://i.pinimg.com/564x/3d/df/7a/3ddf7af1cd59885371ba21d328555fa2.jpg';
+}
